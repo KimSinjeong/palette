1d9d2ec716c083a253bb35449bcf677f119a606a
Shinjeong Kim <2725264@gmail.com>
2019-11-05 05:45:21 +0900

    Implemented homography and extracted useful informations from points and markers.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@
 #include <string>
 #include <opencv2/imgproc.hpp>
 
+#define SCALAR 2
+
 using namespace cv;
 using namespace std;
 
@@ -21,6 +23,11 @@ int screen_width;
 int screen_hieght;
 
 Mat dictionary = Mat(250, (6 * 6 + 7) / 8, CV_8UC4, (uchar*)DICT_6X6_1000_BYTES);
+
+typedef struct coordinate {
+	Point2d px, py, gx, gy, v;
+	Mat badukpan;
+};
 
 Mat getByteListFromBits(const Mat& bits) {
 	// integer ceil
@@ -39,7 +46,6 @@ Mat getByteListFromBits(const Mat& bits) {
 
 			// set bit
 			rot0[currentByte] |= bits.at<uchar>(row, col);
-
 			currentBit++;
 			if (currentBit == 8) {
 				// next byte
@@ -67,7 +73,7 @@ bool identify(const Mat& onlyBits, int& idx, int& rotation) {
 		//각 마커 ID
 		for (unsigned int r = 0; r < 4; r++) {
 			int currentHamming = hal::normHamming(
-				dictionary.ptr(m) + r * candidateBytes.cols,
+				dictionary.ptr(m) + r*candidateBytes.cols,
 				candidateBytes.ptr(),
 				candidateBytes.cols);
 
@@ -85,12 +91,10 @@ bool identify(const Mat& onlyBits, int& idx, int& rotation) {
 	return idx != -1;
 }
 
-void is_marker(Mat frame, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, Point2f& g_origin_pixel, Point2f& p_x_pixel, Point2f& p_y_pixel, Point2f& g_x_pixel, Point2f& g_y_pixel, Mat& t_g_p);
+coordinate is_marker(Mat frame, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, Point2f& g_origin_pixel, Point2f& p_x_pixel, Point2f& p_y_pixel, Point2f& g_x_pixel, Point2f& g_y_pixel, Mat& t_g_p);
 
 Mat find_global_real_coor(Mat input_point, Point2f pap_pix2pap_real, Point2f p_origin_pixel, Point2f g_origin_pixel, Point2f p_x_pixel, Point2f p_y_pixel, Point2f g_x_pixel, Point2f g_y_pixel, Mat t_g_p);
 //find_global_real_coor 을 이용해서 아웃풋 좌표를 내보내주면 됨. + 로봇 좌표 추가해야됨 +시리얼로 내보내기+ 종이 잘 만들기, 고정시키기 +
-
-
 
 int main() {
 
@@ -133,21 +137,15 @@ int main() {
 	while (1)
 	{
 		cap1 >> frame;
-		//myLine_arr line_arr;
-
-		while (1) {//2개의 아루코 마커와 3개의 점이 인식될때까지
-			cap1 >> frame;
-			//imshow("hi", frame); //연산 너무 많아서 안나옴
-			waitKey(10);
-			//if (is_marker(frame, pap_pix2pap_real, p_origin_pixel, g_origin_pixel, p_x_pixel, p_y_pixel, g_x_pixel, g_y_pixel, t_g_p))
-				//continue;
-			is_marker(frame, pap_pix2pap_real, p_origin_pixel, g_origin_pixel, p_x_pixel, p_y_pixel, g_x_pixel, g_y_pixel, t_g_p);
-		}
-
+		//2개의 아루코 마커와 3개의 점이 인식될때까지
+		coordinate coord = is_marker(frame, pap_pix2pap_real, p_origin_pixel, g_origin_pixel, p_x_pixel, p_y_pixel, g_x_pixel, g_y_pixel, t_g_p);
 		cout << "detected 2 aruco markers and 3 points" << endl;
 
-
-		/*
+		// TODO
+		// 1. Go board grid detection
+		// 2. Go stone detection
+		// 3. Derive global location of a go stone.
+		
 		//원점: paper marker, real 12*12 배열 생성(mm 단위)
 		int dis_between_lines = 90 / 11;//mm단위
 		for (int i = 0; i < 12; i++) {
@@ -194,9 +192,7 @@ int main() {
 		}
 		imshow("points", frame);
 
-
-
-
+		/*
 		cout << "dot detction" << endl;
 
 		cap1 >> frame;
@@ -388,9 +384,6 @@ int main() {
 	//serialPort.ClosePort(); //작업이 끝나면 포트를 닫는다
 
 	cout << "end connect" << endl;
-
-
-
 	return 0;
 }
 
@@ -400,7 +393,7 @@ int main() {
 
 
 
-void is_marker(Mat input_image, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, Point2f& g_origin_pixel, Point2f& p_x_pixel, Point2f& p_y_pixel, Point2f& g_x_pixel, Point2f& g_y_pixel, Mat& t_g_p) {
+coordinate is_marker(Mat input_image, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, Point2f& g_origin_pixel, Point2f& p_x_pixel, Point2f& p_y_pixel, Point2f& g_x_pixel, Point2f& g_y_pixel, Mat& t_g_p) {
 	if (!input_image.empty()) {
 		Mat input_gray_image;
 		Mat binary_image;
@@ -477,8 +470,6 @@ void is_marker(Mat input_image, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, 
 
 			//otsu 방법으로 이진화를 적용한다. 
 			threshold(marker_image, marker_image, 125, 255, THRESH_BINARY | THRESH_OTSU);
-
-
 
 			//마커의 크기는 6, 검은색 태두리를 포함한 크기는 8
 			//마커 이미지 테두리만 검사하여 전부 검은색인지 확인한다. 
@@ -600,9 +591,7 @@ void is_marker(Mat input_image, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, 
 
 		drawKeypoints(input_gray_image, keypoints, input_image, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-
 		//imshow("blob", input_image);
-
 
 		for (int i = 0; i < detectedMarkers.size(); i++)
 		{
@@ -668,14 +657,12 @@ void is_marker(Mat input_image, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, 
 		// 첫번째 방법 (paper origin을 기준으로 한 projection matrix 하나만 구한다.)
 		// 이후 global origin가 transform된 좌표를 구해서 
 
-
 		// 두번째 방법(projection matrix 를 두개 구한다.)
 		// 사용자가 둔 점은 paper origin 을 기준으로 projection matrix 한걸로 추적
 		// 로봇이 둘 실제 좌표는 globla origin 을 기준으로 projection matrix 계산 한 걸로 추적
 
-
 		// 세번째 방법 (paper origin을 기준으로 projection matrix 구한 image 에서 다시 global 원점을 기준으로 한 transform을 구한다.)
-
+		
 		// transMat_paper 행렬 구하기		
 		Point TopLeft = Point(p_origin_pixel.x, p_origin_pixel.y);
 		Point TopRight = p_x_blob;
@@ -694,87 +681,42 @@ void is_marker(Mat input_image, Mat& pap_pix2pap_real, Point2f& p_origin_pixel, 
 		src[2] = Point2f(BottomRight.x, BottomRight.y);
 		src[3] = Point2f(BottomLeft.x, BottomLeft.y);
 
-		dst[0] = Point2f(0, 0);
-		dst[1] = Point2f(122.0 - 1.0, 0);
-		dst[2] = Point2f(122.0 - 1.0, 122.0 - 1.0);
-		dst[3] = Point2f(0, 122.0 - 1.0);
-
+		dst[0] = SCALAR * Point2f(0, 0);
+		dst[1] = SCALAR * Point2f(122.0, 0);
+		dst[2] = SCALAR * Point2f(122.0, 122.0);
+		dst[3] = SCALAR * Point2f(0, 122.0);
+		
 		Mat transMat_paper = getPerspectiveTransform(src, dst);
 		Mat paperFrame;
 
-		warpPerspective(input_image, paperFrame, transMat_paper, Size(300, 300));
+		warpPerspective(input_image, paperFrame, transMat_paper, Size(SCALAR * 122, SCALAR * 122));
+
+		Mat res;
+		if (detectedMarkers.size() == 2) {
+			// Homography matrix transMat_paper, transform origin points
+			Mat pt = (Mat_<double>(6, 3) << p_origin_pixel.x, p_origin_pixel.y, 1, p_x_pixel.x, p_x_pixel.y, 1,
+				p_y_pixel.x, p_y_pixel.y, 1, g_origin_pixel.x, g_origin_pixel.y, 1, g_x_pixel.x, g_x_pixel.y, 1,
+				g_y_pixel.x, g_y_pixel.y, 1);
+			pt = pt.t();
+			res = (transMat_paper * pt).t();
+			// Change to homogeneous
+			for (int i = 0; i < res.size().height; i++) {
+				res.at<double>(i, 0) /= res.at<double>(i, 2);
+				res.at<double>(i, 1) /= res.at<double>(i, 2);
+			}
+		}
 		imshow("perspective", paperFrame);
 
-
-		// transMat_global
-
-
-
-
-		//calculate the global oringin point with projection matrix
-
-
-
-
-		if (detectedMarkers.size() == 2 && p_x_blob.x && p_y_blob.x) {
-
-			/*
-			//종이_픽셀 <-> 종이_실제 ---------------------------------
-
-			Mat p_blob = (Mat_<double>(2, 2) << p_x_blob.x, p_y_blob.x, p_x_blob.y, p_y_blob.y);
-			Mat real_blob = (Mat_<double>(2, 2) << 122.0, 0.0, 0.0, 122.0);
-			//double pap_pix2pap_real[2][2];
-			//Mat pap_pix2pap_real=(Mat_<double>(2,2)<<real_blob*p_blob.inv());
-			pap_pix2pap_real = real_blob * p_blob.inv();
-			//cout << "pap_pix2pap_real"<<pap_pix2pap_real << endl;
-			Mat test_paper_coor = (Mat_<double>(2, 1) << 61, 61);
-
-			//pap_pix2pap_real 잘 되나 확인(paper 중심에 빨간점 찍기)
-			Mat test_blob_coor = pap_pix2pap_real.inv() * test_paper_coor;
-			//cout <<"test_blob_coor"<< test_blob_coor << endl;
-			circle(input_image, Point2f(test_blob_coor.at<double>(0),test_blob_coor.at<double>(1))+p_origin_pixel, 5, Scalar(0, 0, 255), 1);
-			//잘 되네 ㅋㅋ
-
-
-
-
-			//종이_실제 <-> 글로벌_실제 ------------------------------------
-			Mat t_g_p_pixel = (Mat_<double>(2, 1) << (g_origin_pixel - p_origin_pixel).x, (g_origin_pixel - p_origin_pixel).y);
-			t_g_p = pap_pix2pap_real * t_g_p_pixel;
-
-
-			//실험 1: pixel-> global
-			Mat pixel = (Mat_<double>(2, 1) << 100, 100);
-			circle(input_image, Point2f(pixel), 5, Scalar(255, 0, 0), 1);
-			Mat test_paper_pap_pixel = pixel - (Mat_<double>(2, 1) << p_origin_pixel.x, p_origin_pixel.y);
-			Mat test_paper_pap = pap_pix2pap_real * test_paper_pap_pixel;
-			//cout << "test_paper_pap" << test_paper_pap << endl;
-			Mat test_global_pap = test_paper_pap - t_g_p;
-			//cout << "test_global_pap" << test_global_pap << endl;
-			Mat test_global_screen = (Mat_<double>(2, 1) << test_global_pap.at<double>(0) * p_x_pixel.x + test_global_pap.at<double>(1) * p_y_pixel.x, test_global_pap.at<double>(0) * p_x_pixel.y + test_global_pap.at<double>(1) * p_y_pixel.y);
-			//Mat test_global_real = (Mat_<double>(2, 1) << Point2f(test_global_screen).dot(g_x_pixel), Point2f(test_global_screen).dot(g_y_pixel));
-			Mat g_direction = (Mat_<double>(2, 2) << g_x_pixel.x, g_y_pixel.x, g_x_pixel.y, g_y_pixel.y);
-			Mat test_global_real = g_direction.inv()*test_global_screen;
-			//cout <<"test_global_real"<< Point2f(test_global_real) << endl;
-
-
-			//test_global_real = (Mat_<double>(2, 1) << 200, 200);
-			//실험 2 : global->pixel
-			Mat test_global_screen_2 = (Mat_<double>(2, 1) << Point2f(test_global_real).x * (g_x_pixel.x) + Point2f(test_global_real).y * (g_y_pixel.x), Point2f(test_global_real).x * (g_x_pixel.y) + Point2f(test_global_real).y * (g_y_pixel.y));
-			Mat p_direction = (Mat_<double>(2, 2) << p_x_pixel.x, p_y_pixel.x, p_x_pixel.y, p_y_pixel.y);
-			Mat test_global_pap_2 = p_direction.inv() * test_global_screen_2;
-			//Mat test_global_pap_2 = (Mat_<double>(2, 1) << Point2f(test_global_screen_2).dot(p_x_pixel), Point2f(test_global_screen_2).dot(p_y_pixel));// 내적으로 구하기
-			Mat test_paper_pap_2 = test_global_pap_2 + t_g_p;
-			//cout << "test_paper_pap" << test_paper_pap << endl;
-			Mat test_paper_pap_pixel_2 = pap_pix2pap_real.inv() * test_paper_pap_2;
-			circle(input_image, Point2f(test_paper_pap_pixel_2.at<double>(0), test_paper_pap_pixel_2.at<double>(1)) + p_origin_pixel, 5, Scalar(0, 0, 255), 1);
-			//cout << "pixel point" << Point2f(test_paper_pap_pixel_2.at<double>(0), test_paper_pap_pixel_2.at<double>(1)) + p_origin_pixel << endl;
-
-			imshow("aruco and blob", input_image);
-
-			*/
-			//return 1;
-
+		// Coordinate transformation between global and local coordinates.
+		if (detectedMarkers.size() == 2 && keypoints.size() == 3) {
+			coordinate coord;
+			coord.px = Point2d(res.at<double>(1, 0), res.at<double>(1, 1));
+			coord.py = Point2d(res.at<double>(2, 0), res.at<double>(2, 1));
+			coord.gx = Point2d(res.at<double>(4, 0), res.at<double>(4, 1));
+			coord.gy = Point2d(res.at<double>(5, 0), res.at<double>(5, 1));
+			coord.v = Point2d(res.at<double>(0, 0), res.at<double>(0, 1)) - Point2d(res.at<double>(2, 0), res.at<double>(2, 1));
+			coord.badukpan = paperFrame;
+			return coord;
 		}
 
 		else {
